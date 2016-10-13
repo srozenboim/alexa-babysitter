3b8a1cb3050c4e27ee5e1427a70719c1e4fcac0e
Shirley Rozenboim <srozenboim@gmail.com>
2016-10-12 20:25:38 -0700

    Add if statements to yes and no response functions and edit intents

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@
         http://aws.amazon.com/apache2.0/
 
     or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-*/
+    */
 
 /**
  * This sample shows how to create a Lambda function for handling Alexa Skill requests that:
@@ -31,34 +31,9 @@
 var APP_ID = undefined;//replace with 'amzn1.echo-sdk-ams.app.[your-unique-value-here]';
 
 /**
- * Array containing knock knock jokes.
- */
-// var JOKE_LIST = [
-//     {setup: "To", speechPunchline: "Correct grammar is <break time=\"0.2s\" /> to whom.",
-//         cardPunchline: "Correct grammar is 'to whom'."},
-//     {setup: "Beets!", speechPunchline: "Beats me!", cardPunchline: "Beats me!"},
-//     {setup: "Little Old Lady", speechPunchline: "I didn't know you could yodel!",
-//         cardPunchline: "I didn't know you could yodel!"},
-//     {setup: "A broken pencil", speechPunchline: "Never mind. It's pointless.",
-//         cardPunchline: "Never mind. It's pointless."},
-//     {setup: "Snow", speechPunchline: "Snow use. I forgot", cardPunchline: "Snow use. I forgot"},
-//     {setup: "Boo", speechPunchline: "Aww <break time=\"0.3s\" /> it's okay <break time=\"0.3s\" /> don't cry.",
-//         cardPunchline: "Aww, it's okay, don't cry."},
-//     {setup: "Woo", speechPunchline: "Don't get so excited, it's just a joke",
-//         cardPunchline: "Don't get so excited, it's just a joke"},
-//     {setup: "Spell", speechPunchline: "<say-as interpret-as=\"characters\">who</say-as>",
-//         cardPunchline: "w.h.o"},
-//     {setup: "Atch", speechPunchline: "I didn't know you had a cold!", cardPunchline: "I didn't know you had a cold!"},
-//     {setup: "Owls", speechPunchline: "Yes, they do.", cardPunchline: "Yes, they do."},
-//     {setup: "Berry!", speechPunchline: "Berry nice to meet you.", cardPunchline: "Berry nice to meet you."}
-// ];
-
-
-
-/**
  * The AlexaSkill prototype and helper functions
  */
-var AlexaSkill = require('./AlexaSkill');
+ var AlexaSkill = require('./AlexaSkill');
 
 /**
  * BabysitterSkill is a child of AlexaSkill.
@@ -66,8 +41,8 @@ var AlexaSkill = require('./AlexaSkill');
  *
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript#Inheritance
  */
-var BabysitterSkill = function () {
-    AlexaSkill.call(this, APP_ID);
+ var BabysitterSkill = function () {
+  AlexaSkill.call(this, APP_ID);
 };
 
 // Extend AlexaSkill
@@ -77,146 +52,152 @@ BabysitterSkill.prototype.constructor = BabysitterSkill;
 /**
  * Overriden to show that a subclass can override this function to initialize session state.
  */
-BabysitterSkill.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
-    console.log("onSessionStarted requestId: " + sessionStartedRequest.requestId
-        + ", sessionId: " + session.sessionId);
+ BabysitterSkill.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
+  console.log("onSessionStarted requestId: " + sessionStartedRequest.requestId
+    + ", sessionId: " + session.sessionId);
 
     // Any session init logic would go here.
-};
+  };
 
 /**
  * If the user launches without specifying an intent, route to the correct function.
  */
-BabysitterSkill.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
-    console.log("BabysitterSkill onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
+ BabysitterSkill.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+  console.log("BabysitterSkill onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
 
-    initialGreeting(session, response);
+  initialGreeting(session, response);
 };
 
 /**
  * Overriden to show that a subclass can override this function to teardown session state.
  */
-BabysitterSkill.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
-    console.log("onSessionEnded requestId: " + sessionEndedRequest.requestId
-        + ", sessionId: " + session.sessionId);
+ BabysitterSkill.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
+  console.log("onSessionEnded requestId: " + sessionEndedRequest.requestId
+    + ", sessionId: " + session.sessionId);
 
     //Any session cleanup logic would go here.
-};
+  };
 
-BabysitterSkill.prototype.intentHandlers = {
+  BabysitterSkill.prototype.intentHandlers = {
 
-    "YesIntent": function (intent, session, response) {
-        handleTellMeAJokeIntent(session, response);
+    "AMAZON.YesIntent": function (intent, session, response) {
+      yesResponse(session, response);
     },
 
-    "NoIntent": function (intent, session, response) {
-        handleTellMeAJokeIntent(session, response);
+    "AMAZON.NoIntent": function (intent, session, response) {
+      noResponse(session, response);
     },
 
     "AMAZON.HelpIntent": function (intent, session, response) {
-        var speechText = "";
+      var speechText = "";
 
-        switch (session.attributes.stage) {
-            case 0:
-                speechText = "Knock knock jokes are a fun call and response type of joke. " +
-                    "To start the joke, just ask by saying tell me a joke, or you can say exit.";
-                break;
-            case 1:
-                speechText = "You can ask, who's there, or you can say exit.";
-                break;
-            case 2:
-                speechText = "You can ask, who, or you can say exit.";
-                break;
-            default:
-                speechText = "Knock knock jokes are a fun call and response type of joke. " +
-                    "To start the joke, just ask by saying tell me a joke, or you can say exit.";
-        }
+      switch (session.attributes.stage) {
+        case 0:
+        speechText = "Knock knock jokes are a fun call and response type of joke. " +
+        "To start the joke, just ask by saying tell me a joke, or you can say exit.";
+        break;
+        case 1:
+        speechText = "You can ask, who's there, or you can say exit.";
+        break;
+        case 2:
+        speechText = "You can ask, who, or you can say exit.";
+        break;
+        default:
+        speechText = "Knock knock jokes are a fun call and response type of joke. " +
+        "To start the joke, just ask by saying tell me a joke, or you can say exit.";
+      }
 
-        var speechOutput = {
-            speech: speechText,
-            type: AlexaSkill.speechOutputType.PLAIN_TEXT
-        };
-        var repromptOutput = {
-            speech: speechText,
-            type: AlexaSkill.speechOutputType.PLAIN_TEXT
-        };
+      var speechOutput = {
+        speech: speechText,
+        type: AlexaSkill.speechOutputType.PLAIN_TEXT
+      };
+      var repromptOutput = {
+        speech: speechText,
+        type: AlexaSkill.speechOutputType.PLAIN_TEXT
+      };
         // For the repromptText, play the speechOutput again
         response.ask(speechOutput, repromptOutput);
-    },
+      },
 
-    "AMAZON.StopIntent": function (intent, session, response) {
+      "AMAZON.StopIntent": function (intent, session, response) {
         var speechOutput = "Goodbye";
         response.tell(speechOutput);
-    },
+      },
 
-    "AMAZON.CancelIntent": function (intent, session, response) {
+      "AMAZON.CancelIntent": function (intent, session, response) {
         var speechOutput = "Goodbye";
         response.tell(speechOutput);
-    }
-};
+      }
+    };
 
 
 function initialGreeting(session, response) {
-    var speechText = "Welcome to Babysitter. Have you already saved a child?";
+      var speechText = "Welcome to Babysitter. Have you already saved a child?";
 
     //Reprompt speech will be triggered if the user doesn't respond.
     var repromptText = "Please say yes or no";
 
+    session.lastQuestion = "greeting"
+
     var speechOutput = {
-        speech: speechText,
-        type: AlexaSkill.speechOutputType.PLAIN_TEXT
+      speech: speechText,
+      type: AlexaSkill.speechOutputType.PLAIN_TEXT
     };
     var repromptOutput = {
-        speech: repromptText,
-        type: AlexaSkill.speechOutputType.PLAIN_TEXT
+      speech: repromptText,
+      type: AlexaSkill.speechOutputType.PLAIN_TEXT
     };
     response.askWithCard(speechOutput, repromptOutput, "Babysitter", speechText);
-}
+  }
 
 // If user says that they already have saved a child
 function yesResponse(session, response) {
+  if(session.lastQuestion === "greeting") {
     var speechText = "Would you like to edit, delete, or add a new child?";
 
     //Reprompt speech will be triggered if the user doesn't respond.
     var repromptText = "Please say edit, delete, or new";
+  }
 
     var speechOutput = {
-        speech: speechText,
-        type: AlexaSkill.speechOutputType.PLAIN_TEXT
+      speech: speechText,
+      type: AlexaSkill.speechOutputType.PLAIN_TEXT
     };
     var repromptOutput = {
-        speech: repromptText,
-        type: AlexaSkill.speechOutputType.PLAIN_TEXT
+      speech: repromptText,
+      type: AlexaSkill.speechOutputType.PLAIN_TEXT
     };
     response.askWithCard(speechOutput, repromptOutput, "Babysitter", speechText);
-}
+  }
 
 // If user says that they have not yet saved a child
 function noResponse(session, response) {
+  if(session.lastQuestion === "greeting") {
     var speechText = "Would you like to add a new child?";
 
     //Reprompt speech will be triggered if the user doesn't respond.
     var repromptText = "Please say yes or no";
-
+  }
+  
     var speechOutput = {
-        speech: speechText,
-        type: AlexaSkill.speechOutputType.PLAIN_TEXT
+      speech: speechText,
+      type: AlexaSkill.speechOutputType.PLAIN_TEXT
     };
     var repromptOutput = {
-        speech: repromptText,
-        type: AlexaSkill.speechOutputType.PLAIN_TEXT
+      speech: repromptText,
+      type: AlexaSkill.speechOutputType.PLAIN_TEXT
     };
     response.askWithCard(speechOutput, repromptOutput, "Babysitter", speechText);
-}
+  }
 /**
  * Responds to the user saying "Who's there".
  */
-function handleWhosThereIntent(session, response) {
-    var speechText = "";
-    var repromptText = "";
+ function handleWhosThereIntent(session, response) {
+  var speechText = "";
+  var repromptText = "";
 
-    if (session.attributes.stage) {
-        if (session.attributes.stage === 1) {
+  if (session.attributes.stage) {
+    if (session.attributes.stage === 1) {
             //Retrieve the joke's setup text.
             speechText = session.attributes.setup;
 
@@ -224,95 +205,95 @@ function handleWhosThereIntent(session, response) {
             session.attributes.stage = 2;
 
             repromptText = "You can ask, " + speechText + " who?";
-        } else {
+          } else {
             session.attributes.stage = 1;
             speechText = "That's not how knock knock jokes work! <break time=\"0.3s\" /> "
-                + "knock knock";
+            + "knock knock";
 
             repromptText = "You can ask, who's there."
-        }
-    } else {
+          }
+        } else {
 
         //If the session attributes are not found, the joke must restart. 
         speechText = "Sorry, I couldn't correctly retrieve the joke. "
-            + "You can say, tell me a joke";
+        + "You can say, tell me a joke";
 
         repromptText = "You can say, tell me a joke";
-    }
+      }
 
-    var speechOutput = {
+      var speechOutput = {
         speech: '<speak>' + speechText + '</speak>',
         type: AlexaSkill.speechOutputType.SSML
-    };
-    var repromptOutput = {
+      };
+      var repromptOutput = {
         speech: '<speak>' + repromptText + '</speak>',
         type: AlexaSkill.speechOutputType.SSML
-    };
-    response.ask(speechOutput, repromptOutput);
-}
+      };
+      response.ask(speechOutput, repromptOutput);
+    }
 
 /**
  * Delivers the punchline of the joke after the user responds to the setup.
  */
-function handleSetupNameWhoIntent(session, response) {
-    var speechText = "",
-        repromptText = "",
-        speechOutput,
-        repromptOutput,
-        cardOutput;
+ function handleSetupNameWhoIntent(session, response) {
+  var speechText = "",
+  repromptText = "",
+  speechOutput,
+  repromptOutput,
+  cardOutput;
 
-    if (session.attributes.stage) {
-        if (session.attributes.stage === 2) {
-            speechText = session.attributes.speechPunchline;
-            cardOutput = session.attributes.cardPunchline;
-            speechOutput = {
-                speech: '<speak>' + speechText + '</speak>',
-                type: AlexaSkill.speechOutputType.SSML
-            };
+  if (session.attributes.stage) {
+    if (session.attributes.stage === 2) {
+      speechText = session.attributes.speechPunchline;
+      cardOutput = session.attributes.cardPunchline;
+      speechOutput = {
+        speech: '<speak>' + speechText + '</speak>',
+        type: AlexaSkill.speechOutputType.SSML
+      };
             //If the joke completes successfully, this function uses a "tell" response.
             response.tellWithCard(speechOutput, "Wise Guy", cardOutput);
-        } else {
+          } else {
 
             session.attributes.stage = 1;
             speechText = "That's not how knock knock jokes work! <break time=\"0.3s\" /> "
-                + "Knock knock!";
+            + "Knock knock!";
             cardOutput = "That's not how knock knock jokes work! "
-                + "Knock knock!";
+            + "Knock knock!";
 
             repromptText = "You can ask who's there.";
 
             speechOutput = {
-                speech: speechText,
-                type: AlexaSkill.speechOutputType.SSML
+              speech: speechText,
+              type: AlexaSkill.speechOutputType.SSML
             };
             repromptOutput = {
-                speech: repromptText,
-                type: AlexaSkill.speechOutputType.PLAIN_TEXT
+              speech: repromptText,
+              type: AlexaSkill.speechOutputType.PLAIN_TEXT
             };
             //If the joke has to be restarted, this function uses an "ask" response.
             response.askWithCard(speechOutput, repromptOutput, "Wise Guy", cardOutput);
-        }
-    } else {
-        speechText = "Sorry, I couldn't correctly retrieve the joke. "
-            + "You can say, tell me a joke";
+          }
+        } else {
+          speechText = "Sorry, I couldn't correctly retrieve the joke. "
+          + "You can say, tell me a joke";
 
-        repromptText = "You can say, tell me a joke";
+          repromptText = "You can say, tell me a joke";
 
-        speechOutput = {
+          speechOutput = {
             speech: speechText,
             type: AlexaSkill.speechOutputType.PLAIN_TEXT
-        };
-        repromptOutput = {
+          };
+          repromptOutput = {
             speech: repromptText,
             type: AlexaSkill.speechOutputType.PLAIN_TEXT
-        };
-        response.askWithCard(speechOutput, repromptOutput, "Wise Guy", speechOutput);
-    }
-}
+          };
+          response.askWithCard(speechOutput, repromptOutput, "Wise Guy", speechOutput);
+        }
+      }
 
 // Create the handler that responds to the Alexa Request.
 exports.handler = function (event, context) {
     // Create an instance of the WiseGuy Skill.
     var skill = new BabysitterSkill();
     skill.execute(event, context);
-};
+  };
